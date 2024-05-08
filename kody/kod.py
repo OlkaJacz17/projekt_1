4b443b0efed4b3df04f18e1964640c989f1768d8
OlkaJacz17 <aleksandra.jaczewska@o2.pl>
2024-05-08 14:28:15 +0200

    kontynuacja

--- kody/kod.py
+++ kody/kod.py
@@ -1,5 +1,6 @@
 from math import *
 import numpy as np
+import sys
 
 o = object()
 
@@ -30,12 +31,30 @@ class Transformacje:
         self.ee = (2 * self.flat - self.flat ** 2)
         
         
-        def xyz2bhl(self, X, Y, Z,):
-            """" Funkcja zmienia współrządne ortokartezjańskie na współrzędne geodezyjne"""
+    def xyz2plh(self, X, Y, Z):
+            """" Algorytm Hirvonena - algorytm transformacji współrzędnych ortokartezjańskich (x, y, z)
+            na współrzędne geodezyjne długość szerokość i wysokośc elipsoidalna (phi, lam, h). Jest to proces iteracyjny. 
+            W wyniku 3-4-krotneej iteracji wyznaczenia wsp. phi można przeliczyć współrzędne z dokładnoscią ok 1 cm.     
+            Parameters
+            ----------
+            X, Y, Z : FLOAT
+                 współrzędne w układzie orto-kartezjańskim, 
+
+            Returns
+            -------
+            lat
+                [stopnie dziesiętne] - szerokość geodezyjna
+            lon
+                [stopnie dziesiętne] - długośc geodezyjna.
+            h : TYPE
+                [metry] - wysokość elipsoidalna
+            output [STR] - optional, defoulf 
+                dec_degree - decimal degree
+                dms - degree, minutes, sec"""
             p = np.sqrt(X**2 + Y**2)
             f = np.arctan(Z/(p * (1 -self.ee)))
             while True: 
-                N = Np(self,f)
+                N = self.Np(f)
                 h = (p/np.cos(f)) - N
                 fs = f
                 f = np.arctan(Z/(p *(1 - (self.ee * (N/(N + h )))))) 
@@ -44,10 +63,58 @@ class Transformacje:
             l = np.arctan2(Y, X)
             return(f,l,h)
         
-        def Np(self, f): 
-            N = a/np.sqrt(1 - ee * np.sin(f)**2)
+    def Np(self, f): 
+            N = self.a/np.sqrt(1 - self.ee * np.sin(f)**2)
             return(N)
+        
+    def flhXYZ(self, f, l, h):
+            """ Funkcja zmienia współrzędne geodezyjne na ortokartezjańskie"""
+            N = Np(self, f)
+            X = (N + h) * np.cos(f) * np.cos(l)
+            Y = (N + h) * np.cos(f) * np.sin(l)
+            Z = (N * (1-self.ee) + h) * np.sin(f)
+            return(X, Y, Z)
+        
+    def macierzR(f,l):
+             R = np.array([[-np.sin(f)*np.cos(l), -np.sin(l), np.cos(f)*np.cos(l)],
+                           [-np.sin(f)*np.sin(l), np.cos(l), np.cos(f)*np.sin(l)],
+                           [np.cos(f),          0,     np.sin(f)]])
+             return(R)
+        
+    def XYZ2neu(self, x, y, z, x0, y0, z0):
+            f, l, h = self.xyz2bhl
+            dX = array[x-x0,
+                       y-y0,
+                       z-z0]
+            R = self.macierzR(f, l)
+            dx= R.T @ dX
+            return(dx)
+    
+    
+    
+        
+        
+        
+if __name__ == "__main__":
+    # utworzenie obiektu
+    print(sys.argv)
+    model = sys.argv[3]
+    print(model)
+    geo = Transformacje(model = model)
+    # dane XYZ geocentryczne
+    X = 3664940.500; Y = 1409153.590; Z = 5009571.170
+    phi, lam, h = geo.xyz2plh(X, Y, Z)
+    print(phi, lam, h)
+    # phi, lam, h = geo.xyz2plh2(X, Y, Z)
+    # print(phi, lam, h)        
+ 
             
+    if '--plh2xyz' in sys.argv:
+        x, y,z = flhxyz(f, lh)
+        
+        
+        
+    
     
         
         
