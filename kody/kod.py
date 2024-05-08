895611dcb5680f6fa5943cf1e206f0749ce4034d
OlkaJacz17 <aleksandra.jaczewska@o2.pl>
2024-05-08 10:48:11 +0200

    zdefiniowanie klasy

---
+++ kody/kod.py
@@ -0,0 +1,34 @@
+from math import *
+import numpy as np
+
+o = object()
+
+class Transformacje:
+    def __init__(self, model: str = "wgs84"):
+        """
+        Parametry elipsoid:
+            a - duża półoś elipsoidy - promień równikowy
+            b - mała półoś elipsoidy - promień południkowy
+            flat - spłaszczenie
+            ecc2 - mimośród^2
+        + WGS84: https://en.wikipedia.org/wiki/World_Geodetic_System#WGS84
+        + Inne powierzchnie odniesienia: https://en.wikibooks.org/wiki/PROJ.4#Spheroid
+        + Parametry planet: https://nssdc.gsfc.nasa.gov/planetary/factsheet/index.html
+        """
+        if model == "wgs84":
+            self.a = 6378137.0 # semimajor_axis
+            self.b = 6356752.31424518 # semiminor_axis
+        elif model == "grs80":
+            self.a = 6378137.0
+            self.b = 6356752.31414036
+        elif model == "krasowskiego":
+            self.a = 6378245.0
+            self.b = 6356863.019
+        else:
+            raise NotImplementedError(f"{model} model not implemented")
+        self.flat = (self.a - self.b) / self.a
+        self.ecc = sqrt(2 * self.flat - self.flat ** 2) # eccentricity  WGS84:0.0818191910428 
+        self.ecc2 = (2 * self.flat - self.flat ** 2) # eccentricity
+        
+        
+        
